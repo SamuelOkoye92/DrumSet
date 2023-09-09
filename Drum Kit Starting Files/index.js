c04b99fb7a7f47349f39909e1a601e7ab98e72c5
Samuel Okoye <Samuelokoye3310@gmail.com>
2023-09-09 20:12:48 +0100

    Drum set

---
+++ Drum Kit Starting Files/index.js
@@ -0,0 +1,9 @@
+let buttons = document.querySelectorAll("button");
+buttons.addEventListener("click", function(){
+    alert("This is samuel's code");
+});
+
+let buttons = document.querySelectorAll("button");
+buttons.addEventListener("click", function(){
+    alert("This is samuel's code");
+});
