ab0a1332916b21588542d7f6adffacb9ae0e9a67
Samuel Okoye <Samuelokoye3310@gmail.com>
2023-09-11 15:09:56 +0100

    Drum set

--- Drum Kit Starting Files/index.js
+++ Drum Kit Starting Files/index.js
@@ -1,10 +1,51 @@
 let buttons = document.querySelectorAll(".drum");
 let noOfButtons = buttons.length;
-let audio1 = new Audio("sounds/tom-1.mp3");
 
 for (let i = 0; i < noOfButtons; i++) {
         buttons[i].addEventListener("click", function(){
-        audio1.play();
+        let buttonInnerHTML = this.InnerHTML;
+
+        switch(buttonInnerHTML) {
+         case "w":
+            let audio1 = new Audio("sounds/tom-1.mp3");
+          audio1.play();
+          break;
+         
+         case "a":
+          let audio2 = new Audio("sounds/tom-2.mp3");
+          audio2.play();
+          break;
+
+         case "s":
+            let audio3 = new Audio("sounds/tom-3.mp3");
+            audio3.play();
+            break;
+
+         case "d":
+            let audio4 = new Audio("sounds/tom-4.mp3");
+            audio4.play();
+            break;
+            
+         case "j":
+            let audio5 = new Audio("sounds/snare.mp3");
+            audio5.play();
+            break;
+            
+         case "k":
+            let audio6 = new Audio("sounds/crash.mp3");
+             audio6.play();
+             break;  
+
+         case "l":
+            let audio7 = new Audio("sounds/kick-bass.mp3");
+            audio7.play();
+            break;   
+
+
+         default: console.log(buttonInnerHTML);
+
+        }
+
     });
     
 }
